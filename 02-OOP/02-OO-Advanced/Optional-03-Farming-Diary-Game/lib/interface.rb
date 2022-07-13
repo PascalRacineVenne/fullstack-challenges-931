e033075aa18c8cb73ccf439dec6079a8ab19da00
Pascal Racine-Venne <pascalracinevenne@gmail.com>
2022-07-13 16:03:53 -0400

    redid and fun

--- 02-OOP/02-OO-Advanced/Optional-03-Farming-Diary-Game/lib/interface.rb
+++ 02-OOP/02-OO-Advanced/Optional-03-Farming-Diary-Game/lib/interface.rb
@@ -7,4 +7,42 @@ require_relative "cow"
 require_relative "chicken"
 
 # Display the state of the farm (move the line below in the loop when asked)
-Board.new.display
+
+choice = nil
+crops = []
+animals = []
+
+while choice != 'quit'
+  puts "Pick an action: [corn | rice | water | cow | chicken | feed | quit]"
+  print "> "
+  choice = gets.chomp.downcase
+
+  case choice
+  when "corn"
+    puts "Let's plant corn crops!"
+    crops << Corn.new
+  when "rice"
+    puts "Rice crops today!"
+    crops << Rice.new
+  when "water"
+    puts "Adding water to the crops"
+    crops.each { |crop| crop.water! }
+  when "cow"
+    puts "You both a new cow!"
+    animals << Cow.new
+  when "chicken"
+    puts "You both a new chicken!"
+    puts "Is it a male or a female"
+    print "> "
+    gender = gets.chomp.downcase
+    animals << Chicken.new(gender)
+  when "feed"
+    puts "Feeding the animals"
+    animals.each { |animal| animal.feed! }
+  when "quit"
+    puts "See you next time"
+  else
+    puts "I don't know what you mean..."
+  end
+  Board.new.display
+end
