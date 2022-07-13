8b173682149234309a8a933cc73762e6c41a66d1
Pascal Racine-Venne <pascalracinevenne@gmail.com>
2022-07-13 18:13:51 -0400

    fix this inheritance magic

--- 02-OOP/02-OO-Advanced/Optional-04-Tracking-Subclasses/lib/mother.rb
+++ 02-OOP/02-OO-Advanced/Optional-04-Tracking-Subclasses/lib/mother.rb
@@ -1,10 +1,44 @@
+# require "../spec/mother_spec"
+
 class Mother
+
+  def self.inherited(subclass)
+    puts "New subclass: #{subclass}"
+  end
+
   def self.phone_kids
-    ObjectSpace.each_object(Class)
-               .select { |klass| klass < self}
-               .each { |children| children.phone}
+    
   end
 end
 
+class Son < Mother
+  @phoned = false
+
+  class << self
+    attr_accessor :phoned
+  end
+
+  def self.phone
+    self.phoned = true
+  end
+end
+
+class Daughter < Mother
+  @phoned = false
+
+  class << self
+    attr_accessor :phoned
+  end
+
+  def self.phone
+    self.phoned = true
+  end
+end
+
+
 # Daughter and Son are classes inheriting from Mother. They are defined
 # in the specs, no need to create them.
+# p son = Son.new
+p mom = Mother.new
+Mother.inherited(Son)
+Mother.phone_kids
