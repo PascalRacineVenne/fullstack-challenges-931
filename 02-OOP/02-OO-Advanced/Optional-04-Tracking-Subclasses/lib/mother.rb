ec68d850cc13f0b8a74903e06f038d961646eb9f
Pascal Racine-Venne <pascalracinevenne@gmail.com>
2022-07-13 16:03:28 -0400

    that one is fuzzy

--- 02-OOP/02-OO-Advanced/Optional-04-Tracking-Subclasses/lib/mother.rb
+++ 02-OOP/02-OO-Advanced/Optional-04-Tracking-Subclasses/lib/mother.rb
@@ -1,4 +1,9 @@
 class Mother
+  def self.phone_kids
+    ObjectSpace.each_object(Class)
+               .select { |klass| klass < self}
+               .each { |children| children.phone}
+  end
 end
 
 # Daughter and Son are classes inheriting from Mother. They are defined
