7c72d3c85659155a24943726f5598a437fc715b1
Pascal Racine-Venne <pascalracinevenne@gmail.com>
2022-07-12 22:14:16 -0400

    orange redid

--- 02-OOP/01-OO-Basics/03-Basic-OOP/lib/orange_tree.rb
+++ 02-OOP/01-OO-Basics/03-Basic-OOP/lib/orange_tree.rb
@@ -1,3 +1,49 @@
 class OrangeTree
-  # TODO: Implement all the specs defined in the README.md :)
+  attr_reader :age, :height, :fruits
+
+  def initialize
+    @age = 0
+    @height = 0
+    @fruits = 0
+    @dead = false
+  end
+
+  def one_year_passes!
+    unless dead?
+      may_die!
+      grow!
+      fruits!
+    end
+  end
+
+  def grow!
+    @age += 1
+    @height += 1 if @age <= 10
+  end
+
+  def may_die!
+    if @age == 100
+      @dead = true 
+    elsif @age > 50
+      @dead = @age >= rand(51..100)
+    end
+  end
+
+  def fruits!
+    if @age > 5 && @age < 10
+      @fruits = 100
+    elsif @age >= 10 && @age < 15
+      @fruits = 200
+    else
+      @fruits = 0
+    end   
+  end
+
+  def dead?
+    @dead
+  end
+
+  def pick_a_fruit!
+    @fruits -= 1 if @fruits >= 1
+  end
 end
