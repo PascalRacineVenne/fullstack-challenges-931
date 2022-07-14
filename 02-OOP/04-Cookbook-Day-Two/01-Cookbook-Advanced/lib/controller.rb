5f79b7e782e4d0f2db2dc2927f398cdf7361a581
Pascal Racine-Venne <pascalracinevenne@gmail.com>
2022-07-14 17:27:34 -0400

    refactoring in action

--- 02-OOP/04-Cookbook-Day-Two/01-Cookbook-Advanced/lib/controller.rb
+++ 02-OOP/04-Cookbook-Day-Two/01-Cookbook-Advanced/lib/controller.rb
@@ -1,18 +1,14 @@
-require 'open-uri'
-require 'nokogiri'
+require_relative 'search'
 require_relative 'view'
 
 class Controller
-  BASE_URL = "https://www.allrecipes.com/search/results/?search="
-
   def initialize(cookbook)
     @cookbook = cookbook
     @view = View.new
   end
 
   def list
-    recipes = @cookbook.all
-    @view.display(recipes)
+    display
   end
 
   def create
@@ -22,36 +18,14 @@ class Controller
     prep_time = @view.ask_for(:prep_time)
     new_recipe = Recipe.new(name: name, description: description, rating: rating, prep_time: prep_time)
     @cookbook.add_recipe(new_recipe)
-    list
   end
 
   def search
     ingredient = @view.search_for(:ingredient)
-    query = "#{BASE_URL}#{ingredient}"
-    html_doc = URI.open(query).read
-    xml_doc = Nokogiri::HTML(html_doc)
-
-    results = []
-
-    xml_doc.search(".card__recipe").first(5).each do |element|
-      name = element.search(".card__imageContainer a").first.attribute("title").value.strip
-      description = element.search(".card__detailsContainer .card__summary").first.text.strip
-      rating_element = element.search(".card__detailsContainer .review-star-text").first
-      rating = rating_element ? rating_element.text.strip.match(/\d\.?\d*/)[0] : nil
-      recipe_url = element.search(".card__imageContainer a").first.attribute("href").value.strip
-      recipe_html = URI.open(recipe_url).read
-      recipe_doc = Nokogiri::HTML(recipe_html, nil, "utf-8")
-      prep_element = recipe_doc.search(".recipe-meta-item").find do |element|
-        element.text.strip.match?(/prep/i)
-      end
-      prep_time = prep_element ? prep_element.text.strip.match(/prep:\s+(?<prep>\d+)/i)[1] : nil
-
-      results << Recipe.new(name: name, description: description, rating: rating, prep_time: prep_time)
-    end
+    results = Search.new(ingredient).scrape
     @view.display(results)
     index = @view.ask_for_internet(:index)
     @cookbook.add_recipe(results[index])
-    list
   end
 
   def mark_as_done
@@ -64,6 +38,12 @@ class Controller
     list
     index = @view.ask_for(:index).to_i
     @cookbook.remove_recipe(index - 1)
-    list
+  end
+
+  private
+
+  def display
+    recipes = @cookbook.all
+    @view.display(recipes)
   end
 end
---
+++ 02-OOP/04-Cookbook-Day-Two/01-Cookbook-Advanced/lib/search.rb
@@ -0,0 +1,36 @@
+require 'open-uri'
+require 'nokogiri'
+require_relative 'recipe'
+
+class Search
+  BASE_URL = "https://www.allrecipes.com/search/results/?search="
+
+  def initialize(ingredient)
+    @ingredient = ingredient
+  end
+
+  def scrape
+    query = "#{BASE_URL}#{@ingredient}"
+    html_doc = URI.open(query).read
+    xml_doc = Nokogiri::HTML(html_doc)
+
+    results = []
+
+    xml_doc.search(".card__recipe").first(5).each do |element|
+      name = element.search(".card__imageContainer a").first.attribute("title").value.strip
+      description = element.search(".card__detailsContainer .card__summary").first.text.strip
+      rating_element = element.search(".card__detailsContainer .review-star-text").first
+      rating = rating_element ? rating_element.text.strip.match(/\d\.?\d*/)[0] : nil
+      recipe_url = element.search(".card__imageContainer a").first.attribute("href").value.strip
+      recipe_html = URI.open(recipe_url).read
+      recipe_doc = Nokogiri::HTML(recipe_html, nil, "utf-8")
+      prep_element = recipe_doc.search(".recipe-meta-item").find do |element|
+        element.text.strip.match?(/prep/i)
+      end
+      prep_time = prep_element ? prep_element.text.strip.match(/prep:\s+(?<prep>\d+)/i)[1] : nil
+
+      results << Recipe.new(name: name, description: description, rating: rating, prep_time: prep_time)
+    end
+    results
+  end
+end
